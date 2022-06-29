b0e7c8aa48962288682c32669b3863b8082b89ad
Shivam <shivamjaiswal151@gmail.com>
2022-06-30 00:57:00 +0530

    config files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { Provider } from "react-redux";
+import "bootstrap/dist/css/bootstrap.css";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+import store from './store/store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+// After
+// import { createRoot } from 'react-dom/client';
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+  <Provider store={store}>
+    <App tab="home" />
+   </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
